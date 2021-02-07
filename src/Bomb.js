c051862fad9fddd9b00e21a6296a08a91c6d53b9
juan_carlos_meza <juancmezat@gmail.com>
2021-02-06 19:13:09 -0800

    done

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,26 @@
 // your Bomb code here!
+import React, {Component} from 'react'
+
+export default class ImageSlider extends Component {
+
+    // constructor(props) { //props that the component gets from its parent
+    //     super()
+    //     this.state = { //define initial state with a key of 'someKey' set to the 'someValue' prop
+    //     secondsLeft: props.initialCount
+    //     }
+    //   }
+
+    state = {
+        secondsLeft: this.props.initialCount
+    }
+
+
+    render() {
+
+        debugger
+        return (
+
+            <div>{this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`}</div>
+        )
+    }
+}
